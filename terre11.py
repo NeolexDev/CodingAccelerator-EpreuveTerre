fc89ab6c47fb4acde869f1819b9642ea9a177524
Neolex <hascoet.kevin@gmail.com>
2024-12-24 11:08:15 +0100

    fix terre11.py

--- terre11.py
+++ terre11.py
@@ -1,16 +1,6 @@
 """ module to check if number is prime number """
 import sys
 
-def square_root(n):
-    ''' return square root of a number '''
-    res = 1
-    while True:
-        if res*res == n:
-            return res
-        if res > n:
-            return None
-        res+=1
-
 def is_prime(n):
     ''' check if n is prime number '''
     if n < 2 or (n>2 and n%2==0):
